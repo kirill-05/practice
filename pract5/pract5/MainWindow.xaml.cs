b1f60067d372d9378580a1fd1e7783c15afb32d1
kirill-05 <90303010+kirill-05@users.noreply.github.com>
2021-11-11 19:46:24 +0300

    Add files via upload

---
+++ pract5/pract5/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pract5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        readonly Triad triad = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Calculate_Click(object sender, RoutedEventArgs e)
+        {
+            int a = Int32.Parse(value1.Text);
+            int b = Int32.Parse(value2.Text);
+            int c = Int32.Parse(value3.Text);
+            triad.SetParams(a, b, c);
+            OutSum.Text = triad.Sum().ToString();
+        }
+
+        private void Info_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Махмудов Кирилл ИСП-34 \nСоздать класс Triad (тройка положительных чисел). " +
+                " Создать необходимые методы и свойства.Определить метод вычисления суммы чисел. " +
+                "Создать перегруженные методы SetParams, для установки параметров объекта" ,"О программе" , MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+           Close();
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            value1.Clear();
+            value2.Clear();
+            value3.Clear();
+            OutSum.Clear();
+        }
+    }
+}
---
+++ pract5/pract5/Triad.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pract5
+{
+    class Triad
+    {
+        private int number1;
+        private int number2;
+        private int number3;
+
+
+        public int Number1
+        {
+
+            get
+            {
+                return number1;
+            }
+
+            set
+            {
+
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Введите положительное число");
+                }
+                else number1 = value;
+            }
+        }
+
+
+        public int Number2
+        {
+
+            get
+            {
+                return number2;
+            }
+
+            set
+            {
+
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Введите положительное число");
+                }
+                else number2 = value;
+            }
+        }
+
+
+        public int Number3
+        {
+
+            get
+            {
+                return number3;
+            }
+
+            set
+            {
+
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Введите положительное число");
+                }
+                else number3 = value;
+            }
+        }
+
+        public int Sum()
+        {
+            return Number1 + Number2 + Number3;
+        }
+
+        public void SetParams(int newNumber)
+        {
+            Number1 = newNumber;
+            Number2 = newNumber;
+            Number3 = newNumber;
+        }
+
+        public void SetParams(int newNumber1, int newNumber2, int newNumber3)
+        {
+            Number1 = newNumber1;
+            Number2 = newNumber2;
+            Number3 = newNumber3;
+        }
+    }
+}
