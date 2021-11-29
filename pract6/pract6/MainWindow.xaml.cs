7a8e0966e7daba37a35360ef34f7ec023434a2ea
kirill-05 <90303010+kirill-05@users.noreply.github.com>
2021-11-29 19:56:57 +0300

    Add files via upload

---
+++ pract6/pract6/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pract6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Calculate_Click(object sender, RoutedEventArgs e)
+        {
+            int a = Convert.ToInt32(value1.Text);
+            int b = Convert.ToInt32(value2.Text);
+            int c = Convert.ToInt32(value3.Text);
+            Triad triad1 = new Triad(a, b, c);
+            MessageBox.Show($"{triad1.Number1 + triad1.Number2 + triad1.Number3}", "Сумма чисел");
+        }
+
+
+        private void Info_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Махмудов Кирилл ИСП-34 \nИспользовать класс Triad (тройка положительных чисел)." +
+                "Разработать операции определения равенства / неравенства чисел true / false." +
+                "Разработать операции проверки полного равенства / неравенства чисел в триадах(a1, b1, c1) == (a2, b2, c2). " ,
+                "О программе", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            value1.Clear();
+            value2.Clear();
+            value3.Clear();
+            First.Clear();
+            Second.Clear();
+            third.Clear();
+
+        }
+
+        private void Equality_Click(object sender, RoutedEventArgs e)
+        {
+            Triad FirstTriad = new Triad(int.Parse(value1.Text), int.Parse(value2.Text), int.Parse(value3.Text));
+            Triad SecondTriad = new Triad(int.Parse(First.Text), int.Parse(Second.Text), int.Parse(third.Text));
+            MessageBox.Show($"{FirstTriad.IsEqual(SecondTriad)}" ,"Равенство триад", MessageBoxButton.OK);
+
+        }
+    }
+}
---
+++ pract6/pract6/Triad.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pract6
+{
+    class Triad
+    {
+
+        public Triad(int number1, int number2, int number3)
+        {
+            Number1 = number1;
+            Number2 = number2;
+            Number3 = number3;
+        }
+
+        public int Number1 { get; set;}
+        public int Number2 { get; set; }
+        public int Number3 { get; set; }
+
+        
+        public bool IsEqual()
+        {
+            return Number1 == Number2 && Number2 == Number3;
+        }
+
+        public bool IsEqual(Triad anotheTraid)
+        {
+            bool First = Number1 == anotheTraid.Number1;
+            bool Second = Number2 == anotheTraid.Number2;
+            bool Third = Number3 == anotheTraid.Number3;
+
+            return First == Second && Second == Third;
+        }
+
+        public static bool operator true(Triad triad)
+        {
+            if (triad.Number1 > 0 && triad.Number2 > 0 && triad.Number3 > 0)
+                return true;
+            else return false;
+        }
+
+        public static bool operator false(Triad triad)
+        {
+            return triad.Number1 <= 0 && triad.Number2 <= 0 && triad.Number3 <= 0;
+        }
+
+        public static bool operator ==(Triad triad1, Triad triad2)
+        {
+            return triad1.IsEqual(triad2);
+        }
+
+        public static bool operator !=(Triad triad1, Triad triad2)
+        {
+            return !(triad1 == triad2);
+        }
+
+    }
+}
