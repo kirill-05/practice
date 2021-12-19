07c35a068bd9abac4995303b19f05047abd2fd11
kirill-05 <90303010+kirill-05@users.noreply.github.com>
2021-12-19 15:52:58 +0300

    Add files via upload

---
+++ pract15.1/ClassLibrary1/Practice.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib_6
+{
+    public class Practice
+    {
+        public static  (string sqrt , string numbers, string pow) СomputationValues()
+        {
+            Random random = new Random();
+            int value = random.Next(-5, 4);
+            int s;
+            int n;
+            string numbers = string.Empty;
+            string sqrt = string.Empty;
+            string pow = string.Empty;
+            double x = 0;
+            do
+            {
+
+                s = value;
+                value = random.Next(-5, 4);
+                
+                if (value > 0)
+                {
+                    x = Math.Sqrt(value);
+                    sqrt += x.ToString() + " ";
+
+                }
+                if (value < 0)
+                {
+                    x = value * value;
+                    pow += x.ToString() + " "; 
+                }
+                numbers += value.ToString() + " ";
+                n = value;
+            }
+            while (s != n);
+            return (sqrt, numbers , pow);
+        }
+    }
+}
---
+++ pract15.1/WpfApp6/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Lib_6;
+
+namespace WpfApp6
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Generate_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var number = Practice.СomputationValues();
+                generated.Text = number.numbers;
+                result.Text = number.sqrt.ToString();
+                outvalue.Text = number.pow.ToString();
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("Введите корректные данные");
+            }
+        }
+
+        private void Info_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Махмудов Кирилл ИСП-34 \n Генерировать случайные числа Х, распределенные в диапазоне от -5 до 4 и вычислять для чисел > 0 X, а для чисел < 0 функцию x^2. " +
+                "Вычисленияпрекратить, когда подряд появится два одинаковых случайных числа.  " +
+                "На экраннеобходимо выводить сгенерированное число и результат расчета функции наразных строках.", "О программе", MessageBoxButton.OK);
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void generated_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            result.Clear();
+            outvalue.Clear();
+        }
+    }
+}
